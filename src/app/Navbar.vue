f51e4020d97bdb864e38603e0871fe9261ccf153
Claudio Dietrich <crdietrichjr@gmail.com>
2019-09-01 17:45:44 -0300

    add navigation bar

---
+++ src/app/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-navbar
+    type="has-background-secondary"
+    v-bind:fixed-top="true">
+    <template slot="brand">
+      <b-navbar-item href="/">
+        TellYou
+      </b-navbar-item>
+    </template>
+
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a href="#" class="button is-primary is-rounded">
+            {{ $t('navbar.buttons.singUp') }}
+          </a>
+
+          <a href="#" class="button is-light is-rounded">
+            {{ $t('navbar.buttons.logIn') }}
+          </a>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
--- src/i18n/ptBR.js
+++ src/i18n/ptBR.js
@@ -1,3 +1,8 @@
 export default {
-
+  navbar: {
+    buttons: {
+      singUp: 'cadastrar-se',
+      logIn: 'entrar'
+    }
+  }
 }

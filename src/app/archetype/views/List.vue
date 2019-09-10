580318c90290e4dd428472242e99d5baf6cd2c29
Claudio Dietrich <crdietrichjr@gmail.com>
2019-09-10 02:49:28 -0300

    feat: implements archetype list

--- src/app/archetype/views/List.vue
+++ src/app/archetype/views/List.vue
@@ -19,6 +19,41 @@
           </div>
         </div>
       </div>
+
+      <div class="card mb-4"
+        v-for="archetype in archetypes"
+        v-bind:key="archetype.id">
+        <div class="card-header">
+          <div class="card-header-title">
+            {{ archetype.name }}
+            <b-tag
+              class="ml-2"
+              v-bind:type="archetype.isRequired ? 'is-primary' : 'is-secondary'"
+              rounded>
+              {{ archetype.isRequired ? $t('archetype.view.form.label.required') : $t('archetype.view.form.label.optional') }}
+            </b-tag>
+          </div>
+        </div>
+
+        <div class="card-content">
+          {{ archetype.description }}
+        </div>
+      </div>
     </div>
   </section>
 </template>
+
+<script>
+import { db } from '@/libs/firebase'
+
+export default {
+  data () {
+    return {
+      archetypes: []
+    }
+  },
+  firestore: {
+    archetypes: db.collection('archetypes')
+  }
+}
+</script>

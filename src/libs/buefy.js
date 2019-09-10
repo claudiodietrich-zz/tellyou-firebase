244092d9ba1ae8a030c7ace9a291d9d025170458
Claudio Dietrich <crdietrichjr@gmail.com>
2019-09-10 00:06:15 -0300

    fix buefy fontawesome icons

--- src/libs/buefy.js
+++ src/libs/buefy.js
@@ -3,4 +3,18 @@ import Buefy from 'buefy'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.component('vue-fontawesome', FontAwesomeIcon)
-Vue.use(Buefy, { defaultIconComponent: 'vue-fontawesome', defaultIconPack: 'fas' })
+Vue.use(Buefy, {
+  defaultIconComponent: 'vue-fontawesome',
+  defaultIconPack: 'fas',
+  customIconPacks: {
+    fas: {
+      sizes: {
+        default: 'lg',
+        'is-small': '',
+        'is-medium': '2x',
+        'is-large': '3x'
+      },
+      iconPrefix: ''
+    }
+  }
+})
